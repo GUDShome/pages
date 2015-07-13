41ed341930ff9bfc519844a0b3fe2c1197a9fc93
sarumain <sandeeparumain@gmail.com>
2015-07-12 20:38:00 -0400

    Create timhortons.css

---
+++ timhortons.css
@@ -0,0 +1,81 @@
+body {
+	background-image:url(http://i.imgur.com/cAZLC8G.png);
+	background-repeat:no-repeat;
+	background-color: #afe9c6;
+	margin-top:44px;
+	font-family: Roboto;
+}
+
+.banner {
+	position: absolute;
+	margin-left:27%;
+}
+
+.wrapper {
+	width:644px;
+	margin: 0 auto;
+}
+
+.top {
+	width:644px;
+	height:236px;
+	background-image:url(http://i.imgur.com/LHP93Ao.png);
+}
+
+.mid {
+	padding-top:10px;
+	padding-bottom:26px;
+	width:644px;
+	height:100%;
+	text-align: center;
+	background-image:url(http://i.imgur.com/ERL9NfY.png);
+}
+
+.bottom {
+	width:644px;
+	height:10px;
+	background-color: brown;
+}
+
+.orders {
+	margin:0 auto;
+	padding-left:0px;
+	width:350px;
+}
+
+.orders li {
+	border-right:2px solid #f7d54c;
+	border-bottom:2px solid #f7d54c;
+	width: 100%;
+	text-align:center;
+	margin-top:10px;
+	padding: 10px;
+	list-style-type: none;
+	background-color:#f4e5a8;
+}
+
+.remove {
+	font-size:10px;
+	font-family: Roboto;
+	padding: 5px;
+	background-color: #f6c5f7;
+	border-right:2px solid #e694e8;
+	border-bottom:2px solid #e694e8;
+}
+
+.mid p {
+	text-align: center;
+	padding-top: 5px;
+}
+
+
+#addorder {
+	font-size:10px;
+	font-family: Roboto;
+	padding:10px;
+	margin-right: 10px;
+	background-color: #f6c5f7;
+	border-right:2px solid #e694e8;
+	border-bottom:2px solid #e694e8;
+	display:inline-block;
+}
